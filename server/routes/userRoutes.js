ad2f3f72ab7c21b774e3ff91cfe12214d0df603c
Pranav tiwari <91013998+pranav-tiwari-exe@users.noreply.github.com>
2025-08-17 21:09:35 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,14 +4,14 @@ import userAuth from '../middlewares/auth.js'
 
 const userRouter = express.Router()
 
-userRouter.post('/register', registerUser)
+userRouter.post('/api/user/register', registerUser)
 
-userRouter.post('/login', loginUser)
+userRouter.post('/api/user/login', loginUser)
 
-userRouter.get('/credits', userAuth, userCredits)
+userRouter.get('/api/user/credits', userAuth, userCredits)
 
-userRouter.post('/pay-razor', userAuth, paymentRazorpay )
+userRouter.post('/api/user/pay-razor', userAuth, paymentRazorpay )
 
-userRouter.post('/verify-razor', userAuth, verifyRazorPay )
+userRouter.post('/api/user/verify-razor', userAuth, verifyRazorPay )
 
 export default userRouter  
