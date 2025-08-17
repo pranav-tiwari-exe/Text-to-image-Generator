2dcccda48125fefd539dcd772524f5c5123e5c0d
Pranav tiwari <91013998+pranav-tiwari-exe@users.noreply.github.com>
2025-08-17 21:14:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,20 +6,28 @@ import userRouter from './routes/userRoutes.js'
 import imageRouter from './routes/imageRoutes.js'
 
 const port = process.env.PORT || 5000;
-
-const app=express()
+const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(userRouter)
-app.use(imageRouter)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+app.use(userRouter) 
+app.use(imageRouter) 
+
+app.get('/api', (req, res) => {
+  res.send('Hello from API!');
 })
 
-await connectDB().then(() => {app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-})});
+app.use((req, res, next) => {
+  console.log(`[Vercel DEBUG] Path: ${req.path}, Method: ${req.method}`);
+  res.status(404).json({ message: 'Route not found in Express app' });
+});
+
+await connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  })
+});
 
 export default app;
